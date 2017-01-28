3ca14abd99a7a7aad9c5e9855e8af86f99a926f7
megan liken <meganliken@megans-MBP-2.attlocal.net>
2017-01-27 23:04:30 -0600

    shape constructor

--- shapes.js
+++ shapes.js
@@ -21,5 +21,31 @@ Shapes.prototype.get_type = function(){
 }
 
 //adding a method to the constructor so that it will be global to all functions
-Shapes.prototype.get_c
+Shapes.prototype.get_totalLength = function () {
+	var length = 0;
+	if (this.type === 'Triangle'){
+		length = this.side1 + this.side2 + this.side3;
+	} else if (this.type === 'Square'){
+		length = 4 * this.side;
+	} else if (this.type === "Pentagon"){
+		length = this.side1 + this.side2 + this.side3 + this.side4 + this.side5;
+
+	} return length;
+};
+
+//where inheritance occurs from shape to square
+Square.prototype = new Shape ();
+// Otherwise instances of square would have a constructor of shape
+Square.prototype.constructor = Square;
+
+//shape to triangle
+Triangle.prototype = new Shape ();
+Triangle.prototype.constructor = Triangle;
+
+//shape to pentagon
+Pentagon.prototype = new Shape();
+Pentagon.prototype.constructor = Pentagon;
+
+
+
 
