f52ae2fb8c55075bcd552e7bbaeaa37fba2e1959
megan liken <meganliken@megans-MBP-2.attlocal.net>
2017-01-27 23:08:30 -0600

    triangle

--- triangle.js
+++ triangle.js
@@ -1,1 +1,16 @@
 'strict'
+
+var Shapes = require('./Shapes.js');
+
+//create triangle constructor using length
+function Triange (length1, length2, length3) {
+
+	//properties
+	this.type = "Triangle";
+	this.side1 = length1;
+	this.side2 = length2;
+	this.side3 = length3;
+}
+
+//export the triangle function
+module.exports = Triangle;
