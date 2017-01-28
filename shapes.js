9e2aedae568e3a3fe6bdc30bbcce8ce630c8d1d6
megan liken <meganliken@megans-MBP-2.attlocal.net>
2017-01-27 23:31:11 -0600

    console logs

--- shapes.js
+++ shapes.js
@@ -46,6 +46,29 @@ Triangle.prototype.constructor = Triangle;
 Pentagon.prototype = new Shape();
 Pentagon.prototype.constructor = Pentagon;
 
+//creating variables of each constructor
+var triangle1 = new Triangle(3,4,5);
+var square1 = new Square (4);
+var pentagon1 = Pentagon (1,2,3,4,5);
+var shape = new Shape ("shape");
+
+//getting different types
+console.log (triangle1.get_type());
+console.log(triangle1.get_totalLength());
+console.log(square1.get_type());
+console.log (square1.get_totalLength());
+console.log(pentagon1.get_type());
+console.log(pentagon1.get_totalLength());
+console.log(shape);
+
+// double instanceof works 
+console.log(triangle1 instanceof Triangle);
+console.log (square1 instanceof Square);
+console.log(pentagon1 instanceof Pentagon);
+console.log(shape instanceof Shape);
+
+
+
 
 
 
